ca064fa3fdf24c603b1560ce6bbcac219ac1b94e
aalinabilal <aalina.bilal@gmail.com>
2021-06-19 19:47:46 +0500

    Some really basic (non-relevant) changes because pycharm can be annoying

--- src/model.py
+++ src/model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+
 class Model(nn.Module):
     def __init__(self, n_outputs):
 
@@ -13,10 +14,10 @@ class Model(nn.Module):
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
 
             nn.ReLu(),
-            
+
             # First max pooling layer
             nn.MaxPool2d(2),
-            #nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
+            # nn.MaxPool2d(kernel_size=2, stride=2, padding=1),
 
             # More convolutional layers
             nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False),
@@ -24,12 +25,11 @@ class Model(nn.Module):
 
             # Second max pooling layer
             nn.MaxPool2d(2),
-            #nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
-            
+            # nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+
             # Finally, 2 fully connected (linear) layers
             nn.Linear(256, 100),
             nn.Linear(100, n_outputs))
-        
 
     def forward(self, x):
         # Perform the forward pass
--- src/visualize.py
+++ src/visualize.py
@@ -3,11 +3,11 @@ import numpy as np
 
 
 def plot_history(hist, loss_name='loss', path=None):
-    '''
+    """
     Plot the training history of a model.
 
     hist: The training history
-    '''
+    """
     import matplotlib.pyplot as plt
     plt.figure()
     plt.plot(hist.epoch, hist.history[loss_name], '.-')
